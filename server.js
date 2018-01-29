a0d1f92a1c4842c8e16a2442b51658d194d03349
lukasz341 <lukasz341@tlen.pl>
2018-01-29 20:30:02 +0100

    zmiana adresu bazy danych na mlab

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://lukasz:kodilla2018@ds119268.mlab.com:19268/nodeappdatabase', {
     useMongoClient: true
 });
 
